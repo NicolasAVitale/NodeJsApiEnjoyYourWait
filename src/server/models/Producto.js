694dbaaf076c80a1c8058f56e59f64afee72d807
Micaela <micaela.sch.aquino@gmail.com>
2020-06-06 09:37:44 -0300

    cambio nombre de variable

--- src/server/models/Producto.js
+++ src/server/models/Producto.js
@@ -5,17 +5,17 @@ class Producto{
     constructor(detalle, nombre, precio, idPrecio, imagen = "null"){
         this.nombre = nombre
         this.precio = precio
-        this.idPrecio = idPrecio
+        this.idTipo = idPrecio
         this.imagen = imagen
 
     }
 
     static validar(producto){
         const productoSchema = {
-            nombre: Joi.string().alphanum().min(1).required(),
+            nombre: Joi.string().alphanum().min(150).required(),
             precio: Joi.number().integer().min(0).required(),
             idTipo: Joi.number().integer().min(1).required(),
-            imagen: Joi.string().alphanum()
+            imagen: Joi.string().alphanum().min(100)
         }
 
         const { error } = Joi.validate(producto, productoSchema)
