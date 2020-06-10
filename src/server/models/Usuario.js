cbbf58ff3552defebdd028ddde76a0834a4a29be
Micaela <micaela.sch.aquino@gmail.com>
2020-06-09 22:42:01 -0300

    se mejora la validación para fechas

--- src/server/models/Usuario.js
+++ src/server/models/Usuario.js
@@ -1,8 +1,10 @@
-import Joi from "@hapi/joi"
+import JoiBase from "@hapi/joi" 
+import JoiDate from "@hapi/joi-date"
+const Joi = JoiBase.extend(JoiDate) // extend Joi with Joi Date
 
 class Usuario {
 
-    constructor(dni, nombre, apellido, email, fechaNacimiento, contrasena, idRol,fechaPrimerIngreso) {
+    constructor(dni, nombre, apellido, email, fechaNacimiento, contrasena, idRol,fechaPrimerIngreso,activo) {
         this.dni = dni
         this.nombre = nombre
         this.apellido = apellido
@@ -11,22 +13,25 @@ class Usuario {
         this.contrasena = contrasena
         this.idRol = idRol
         this.fechaPrimerIngreso = fechaPrimerIngreso
+        this.activo = activo
     }
 
     static validar(usuario) {
 
-        const usuarioSchema = {
+        const usuarioSchema = Joi.object({
             dni: Joi.number().integer().min(1).max(99999999).required(),
             nombre: Joi.string().alphanum().min(1).max(150).required(),
             apellido: Joi.string().alphanum().min(1).max(150).required(),
-            email: Joi.string().email().required(),
-            fechaNacimiento: Joi.date().required(),
+            email: Joi.string().email(),
+            fechaNacimiento: Joi.date().format('YYYY-MM-DD'),
             contrasena: Joi.string().alphanum().min(0).max(100),
             idRol: Joi.number().integer().min(1).required(),
-            fechaPrimerIngreso: Joi.date().required()
-        }
+            fechaPrimerIngreso: Joi.date().format('YYYY-MM-DD'),
+            activo: Joi.number().integer().min(1)
+        })
 
         const { error } = usuarioSchema.validate(usuario)
+
         if (error) {
             throw error
         }
