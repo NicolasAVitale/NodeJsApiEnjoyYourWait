78868b37b848c97264fbf5480a0418a85c951ba8
Micaela <micaela.sch.aquino@gmail.com>
2020-06-16 22:09:39 -0300

    se ajusta el config para soportar base de test

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-const currentEnv = process.env.ENV || 'prod'
+// const currentEnv = process.env.ENV || 'prod'
+const currentEnv = process.env.ENV || 'dev'
 
 const envs = {
   prod: {
     port: process.env.PROD_PORT,
-    // mode: process.env.PROD_MODE,
-    mode: "db",
+    mode: process.env.PROD_MODE,
+    // mode: "db",
     db: {
       user: process.env.PROD_MSSQL_DB_USER,
       password: process.env.PROD_MSSQL_DB_PASS,
@@ -26,8 +27,20 @@ const envs = {
     port: process.env.DEV_PORT,
     mode: process.env.DEV_MODE,
     db: {
+      user: process.env.DEV_MSSQL_DB_USER,
+      password: process.env.DEV_MSSQL_DB_PASS,
+      database: process.env.DEV_MSSQL_DB_DATABASE,
+      server: process.env.DEV_MSSQL_DB_HOST,
       client: process.env.DEV_DB_CLIENT,
-      dbPath: process.env.DEV_DB_PATH
+      pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+      },
+      options: {
+        encrypt: false,
+        enableArithAbort: true
+      }
     }
   }
 }
